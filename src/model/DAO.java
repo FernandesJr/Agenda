097ae0c07e827eb1b3e32620b867053bc9176366
Fernandes Júnior <fernaandes.jr@gmail.com>
2021-02-09 11:45:04 -0300

    Connection DB

--- src/model/DAO.java
+++ src/model/DAO.java
@@ -1,5 +1,30 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	
+	private Connection conexao = null;
+	
+	private String drive = "com.mysql.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/db_agenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
+	private String passoword = "";
+	
+	private Connection conexao() {
+		
+		try {
+			
+			Class.forName(drive);
+			conexao = DriverManager.getConnection(url, user, passoword);
+			
+		} catch (Exception e) {
+			System.out.println("ERRO:" + e);
+		}
+		
+		
+		return conexao;
+	}
+	
 }
